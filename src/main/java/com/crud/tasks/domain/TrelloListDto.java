c38225ff98d1f8aafa5ed93ec5b433382b7ff258
Maciej Kupras <maciejkupras@wp.pl>
2021-07-07 23:02:27 +0200

    22.3_Rozszerzenie klasy odpowiedzi

--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -1,27 +1,19 @@
 package com.crud.tasks.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class TrelloListDto {
 
     @JsonProperty("id")
-    private String name;
-
-    @JsonProperty("name")
     private String id;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
-
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("closed")
+    private boolean isClosed;
 }
-
