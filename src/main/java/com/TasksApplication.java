8211bf595370849b46780d8cadf1705547fd26ca
Maciej Kupras <maciejkupras@wp.pl>
2021-07-12 13:01:12 +0200

    First attempt to Heroku

--- src/main/java/com/TasksApplication.java
+++ src/main/java/com/TasksApplication.java
@@ -13,12 +13,12 @@ public class TasksApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(com.TasksApplication.class);
+    }
 }
-
-
-//   @Override
-//   protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-//       return applicationBuilder.sources(com.TasksApplication.class);
-//   }
 
 
