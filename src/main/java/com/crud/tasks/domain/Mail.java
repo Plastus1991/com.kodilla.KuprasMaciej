2425497316908c64682ea4998e21c5e765eb99b5
Maciej Kupras <maciejkupras@wp.pl>
2021-07-16 13:56:38 +0200

    23.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.mail.SimpleMailMessage;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final String toCc;
+
+}
---
+++ src/test/java/com/crud/tasks/service/TestSimpleEmailService.java
@@ -0,0 +1,50 @@
+package com.crud.tasks.service;
+
+
+import com.crud.tasks.domain.Mail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+
+@ExtendWith(MockitoExtension.class)
+public class TestSimpleEmailService {
+
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+
+        //Given
+        Mail mail = new Mail("test@test.com", "Test", "Test Message", "mail");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+        //When
+        simpleEmailService.send(mail);
+
+        //Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+
+
+    }
+}
